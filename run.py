eab5b79d63c897335287c2b33752de8cddbc5226
dhardi <denishardi10@outlook.com>
2024-02-04 12:16:19 +0000

    menu created validated data function in progress

--- run.py
+++ run.py
@@ -1,3 +1,39 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random 
+import pyfiglet
+
+
+T = "Miner Swep"
+game_title = pyfiglet.figlet_format(T)
+"""
+Creates The Title of the game with ASCII Art
+"""
+
+
+def game_menu():
+    """
+    Menu of the Game 
+    """
+    print (game_title)
+    print("Press S to Start\n")
+    print("Press H to How to Play\n")
+    selected = input("Enter with S or H\n")
+    Game_option_validated(selected)
+
+def Game_option_validated(data_str):
+    """
+    Validated an input press by the user
+    """
+    while True:
+        print ("Command Invalid please Try again\n")
+        if data_str == "s":
+            print("Game Screen")
+            break
+        elif data_str == "h":
+            print("Tutorial Screen")
+            break
+    
+
+def main ():
+    game_menu()
+    
+main()
