0fcac04a8ddce01967ed037cb3046c2ecebd2578
dhardi <denishardi10@outlook.com>
2024-02-04 15:25:01 +0000

    fixing create board function

--- run.py
+++ run.py
@@ -25,7 +25,8 @@ def Game_option_validated(data_str):
     Validated an input press by the user
     """
     if data_str == "s":
-        print("Game Screen")
+        clearConsole()
+        game_costruct ()
             
     elif data_str == "h":
         print("Tutorial Screen")
@@ -35,20 +36,52 @@ def Game_option_validated(data_str):
             print (game_title)
             x_str = input("Enter with S or H\n")
             if x_str == "s":
-                print("Game Screen")
+                clearConsole()
+                game_costruct ()
                 break
             elif x_str == "h":
                 ("Tutorial Screen")
                 break
             
 def clearConsole():
+    """
+    clean the screen for the user code from https://www.delftstack.com/howto/python/python-clear-console/
+    """
     command = "clear"
     if os.name in ("nt", "dos"):  # If Machine is running on Windows, use cls
         command = "cls"
     os.system(command)  
 
+
+def game_costruct ():
+  row_data =[]
+  colum_data =[]  
+  print ("example 3,5\n")
+  data_input = input("Please select row and Colum\n")
+  """raw_data = data_input.split()"""
+  print(data_input)
+  create_board()
+
+
+
+def create_board():
+    
+    for i in range(9):
+       print("[ ]"+"[ ]"+"[ ]"+"[ ]"+"[ ]"+"[ ]"+"[ ]"+ "[ ]") 
+        
+           
+
+        
+
+
+
+
+
+
+
 def main ():
     clearConsole()
     game_menu()
+    
     
 main()
