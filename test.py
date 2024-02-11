0af79814751285c9147d766214aba19bc9b52a50
dhardi <denishardi10@outlook.com>
2024-02-11 17:37:59 +0000

    main board create

--- test.py
+++ test.py
@@ -10,3 +10,26 @@ row_g = ["x","x","x","x","x","x","x","8"]
 row_h = ["x","x","x","x","x","x","x","8"]
 print("   1 |  2 |  3 |  4 |  5 |  6 |  7 |  8 ")
 print(f"1{row_a}\n2{row_b}\n3{row_c}\n4{row_d}\n5{row_e}\n6{row_f}\n7{row_g}\n8{row_h}\n")
+
+def main_board():
+    a=[]
+    b=[]
+    c=[]
+    d=[]
+    e=[]
+    f=[]
+    g=[]
+    h=[]
+    for i in range(9):
+        a.append(i)
+        b.append(i)
+        c.append(i)
+        d.append(i)
+        e.append(i)
+        f.append(i)
+        g.append(i)
+        h.append(i)
+    print(f"{a}\n{a}\n{a}\n{a}\n{a}\n{a}\n{a}\n{a}")
+    
+
+main_board()
