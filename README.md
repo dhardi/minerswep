1e1f070d36594e9b7647b42b8d2a1f0521dc2375
dhardi <denishardi10@outlook.com>
2024-02-18 10:23:22 +0000

    fix validation method

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ MinerSwep is a game similar to minesweeper but with some changes made by me to m
 
 ## How To Play
 
- The game starts with a board with 8 columns and 8 rows. 4 bombs are planted randomly across the board and your duty is to guess where it will be safe to step with eight attempts. If you step on a bomb you lose.
+ The game starts with a board with 8 columns and 8 rows. 4 bombs are planted randomly across the board and your duty is to guess where it will be safe to step . If you step on a bomb you lose.
 
 ### Existing Features
   
@@ -52,10 +52,7 @@ MinerSwep is a game similar to minesweeper but with some changes made by me to m
 
 ## Testing 
 
-During my project I set the objective of making a pokemon game with a first generation of 150 pokemons. The first challenge was to create a system that would generate a random value for each attribute and assign it to all 150 pokemons.
-After this challenge, the gameplay would come, so I created a number comparison system that in the end would compare the highest value of the attribute. To test the gameplay, I had my eight-year-old son play and explore the game.I also put my wife to play and it was vitally important for someone who is not familiar with games. 
-The game will guide you how to play, for players who still have questions about how to play, I have implemented a how to play section.
-I had a lot of problems with the game mechanics when adding events and how to compare the selected object. These problems helped me learn how the event click system works best and the type of variable we should use, but the biggest challenge was making the CPU selects a random card and a random attribute value without repeating for the entire cycle after that the array system was absorbed by me.
+
 
 ### Validator Testing 
 
@@ -72,4 +69,4 @@ I had a lot of problems with the game mechanics when adding events and how to co
 
 ## Credits
 
- 
+ -Firstly, I would like to mention my tutor who was a great help to my development, I would like to thank my wife for her help with color choices. Below I will provide the links to the content to produce the website.
--- test.py
+++ test.py
@@ -5,26 +5,27 @@ import pyfiglet
 T = "Miner Swep"
 G = "Game Tutorial"
 game_title = pyfiglet.figlet_format(T)
-game_tuto =  pyfiglet.figlet_format(G)
+game_tuto = pyfiglet.figlet_format(G)
 
 
-matrix_invisible =[]
+matrix_invisible = []
 """
 display of board
 """
-row_a = ["x","x","x","x","x","x","x","x"]
-row_b = ["x","x","x","x","x","x","x","x"]
-row_c = ["x","x","x","x","x","x","x","x"]
-row_d = ["x","x","x","x","x","x","x","x"]
-row_e = ["x","x","x","x","x","x","x","x"]
-row_f = ["x","x","x","x","x","x","x","x"]
-row_g = ["x","x","x","x","x","x","x","x"]
-row_h = ["x","x","x","x","x","x","x","9"]
+row_a = ["x", "x", "x", "x", "x", "x", "x", "x"]
+row_b = ["x", "x", "x", "x", "x", "x", "x", "x"]
+row_c = ["x", "x", "x", "x", "x", "x", "x", "x"]
+row_d = ["x", "x", "x", "x", "x", "x", "x", "x"]
+row_e = ["x", "x", "x", "x", "x", "x", "x", "x"]
+row_f = ["x", "x", "x", "x", "x", "x", "x", "x"]
+row_g = ["x", "x", "x", "x", "x", "x", "x", "x"]
+row_h = ["x", "x", "x", "x", "x", "x", "x", "9"]
+
 
 def main():
-       
+
     game_menu()
-    
+
 
 def game_tutorial():
     """
@@ -55,14 +56,14 @@ def Game_option_validated(data_str):
     if data_str == "s":
         clearConsole()
         main_board()
-            
+
     elif data_str == "h":
         clearConsole()
         game_tutorial()
     else:
         while True:
             clearConsole()
-            print (game_title)
+            print(game_title)
             x_str = input("Enter with S or H\n")
             if x_str == "s":
                 clearConsole()
@@ -73,23 +74,16 @@ def Game_option_validated(data_str):
                 main_board()
                 break
 
+
 def game_menu():
     """
-    Menu of the Game 
+    Menu of the Game
     """
-    print (game_title)
+    print(game_title)
     print("Press S to Start\n")
     print("Press H to How to Play\n")
     selected = input("Enter with S or H\n")
     Game_option_validated(selected)
-
-
-
-
-
-
-
-
 
 
 def main_board():
@@ -97,15 +91,16 @@ def main_board():
     create a second board it will not be visible to the player
     """
     print("   1 |  2 |  3 |  4 |  5 |  6 |  7 |  8 ")
-    print(f"1{row_a}\n2{row_b}\n3{row_c}\n4{row_d}\n5{row_e}\n6{row_f}\n7{row_g}\n8{row_h}\n")
-    a=[]   
-    b=[]
-    c=[]
-    d=[]
-    e=[]
-    f=[]
-    g=[]
-    h=[]
+    print(f"1{row_a}\n2{row_b}\n3{row_c}\n4{row_d}\n5{
+          row_e}\n6{row_f}\n7{row_g}\n8{row_h}\n")
+    a = []
+    b = []
+    c = []
+    d = []
+    e = []
+    f = []
+    g = []
+    h = []
     for i in range(9):
         a.append(i)
         b.append(i)
@@ -115,12 +110,20 @@ def main_board():
         f.append(i)
         g.append(i)
         h.append(i)
-    mix_numbers(a,b,c,d,e,f,g,h)        
+    mix_numbers(a, b, c, d, e, f, g, h)
 
 
-def mix_numbers(list_a,list_b,list_c,list_d,list_e,list_f,list_g,list_h):  
+def mix_numbers(
+        list_a,
+        list_b,
+        list_c,
+        list_d,
+        list_e,
+        list_f,
+        list_g,
+        list_h):
     """
-    it will mix the number and 8 will be our bomb 
+    it will mix the number and 8 will be our bomb
     """
     global matrix_invisible
     random.shuffle(list_a)
@@ -130,84 +133,98 @@ def mix_numbers(list_a,list_b,list_c,list_d,list_e,list_f,list_g,list_h):
     random.shuffle(list_e)
     random.shuffle(list_f)
     random.shuffle(list_g)
-    random.shuffle(list_h)   
-    #new_board(list_a,list_b,list_c,list_d,list_e,list_f,list_g,list_h)
-    matrix_invisible =[list_a,list_b,list_c,list_d,list_e,list_f,list_g,list_h]
+    random.shuffle(list_h)
+    matrix_invisible = [
+        list_a,
+        list_b,
+        list_c,
+        list_d,
+        list_e,
+        list_f,
+        list_g,
+        list_h]
     print(matrix_invisible)
-    #new_board(matrix_invisible)
     choice_Play(matrix_invisible)
-   
-#def new_board(new_matrix_invisible):
-    """
-    show the board invisible it will be delete this function
-    """
-   # matrix_invisible =[new_list_a,new_list_b,new_list_c,new_list_d,new_list_e,new_list_f,new_list_g,new_list_h]
-    
-    
-    #print(f"{new_list_a}\n{new_list_b}\n{new_list_c}\n{new_list_d}\n{new_list_e}\n{new_list_f}\n{new_list_g}\n{new_list_h}\n")
-    
+
+
 def choice_Play(mtx_data):
     """
     it will get the input and make tuples and put it on a list
     """
-    guess_played =[] 
-    
+    guess_played = []
+
     while True:
         try:
-         num1, num2 = map(int, input("Enter two numbers separated by a space: \n").split())
-         input_play = (num1 - 1 , num2 - 1)
-         guess_played.append(input_play)
-         search_board(guess_played,mtx_data)
-         break
-    
+            num1, num2 = map(
+                int, input("Enter two numbers separated by a space: \n").split())
+            input_play = (num1 - 1, num2 - 1)
+            guess_played.append(input_play)
+            search_board(guess_played, mtx_data)
+            break
 
-        except:
+        except BaseException:
             if num1 > 8 and num2 > 8:
-              num1, num2 = map(int, input("Enter two numbers separated by a space: \n").split())
+                num1, num2 = map(
+                    int, input("Enter two numbers separated by a space: \n").split())
             else:
-                input_play = (num1 - 1 , num2 - 1)
+                input_play = (num1 - 1, num2 - 1)
                 guess_played.append(input_play)
-                search_board(guess_played,mtx_data)
-    
-        finally:
-            num1, num2 = map(int, input("Enter two numbers separated by a space: \n").split())
-            if type(num1)== str and type(num2)==str:
-                choice_Play(mtx_data)
-    
-    
+                search_board(guess_played, mtx_data)
 
-def search_board(pin_board,mtx_data_ava):
+        except BaseException:
+            if input_play in guess_played:
+                num1, num2 = map(
+                    int, input("Enter two numbers separated by a space: \n").split())
+                   
+            
+
+        finally:
+            num1, num2 = map(
+                int, input("Enter two numbers separated by a space: \n").split())
+            if type(num1) == str and type(num2) == str:
+                choice_Play(mtx_data)
+
+
+def search_board(pin_board, mtx_data_ava):
     """
-    it will pin on the board the location of guess played and also check if there is a bomb 
+    it will pin on the board the location of guess played and also check if there is a bomb
     """
     global row_a, row_b, row_c, row_d, row_e, row_f, row_g, row_h, guess_played
-    matrix =[row_a,row_b ,row_c ,row_d ,row_e ,row_f ,row_g ,row_h]    
-    row_guess, column_guess  = pin_board[-1] # the negative value is to take the last input from the list 
-    
-    if mtx_data_ava[row_guess ][column_guess] == 8:
+    matrix = [row_a, row_b, row_c, row_d, row_e, row_f, row_g, row_h]
+    # the negative value is to take the last input from the list
+    row_guess, column_guess = pin_board[-1]
+
+    if mtx_data_ava[row_guess][column_guess] == 8:
         clearConsole()
-        matrix[row_guess ][column_guess] = u"\U0001F4A3"
+        matrix[row_guess][column_guess] = u"\U0001F4A3"
         print("you lost it")
         print("   1 |  2 |  3 |  4 |  5 |  6 |  7 |  8 ")
-        print(f"{matrix[0]}\n {matrix[1]}\n {matrix[2]}\n{matrix[3]}\n{matrix[4]}\n{matrix[5]}\n{matrix[6]}\n{matrix[7]}\n")
+        print(
+            f"{
+                matrix[0]}\n {
+                matrix[1]}\n {
+                matrix[2]}\n{
+                    matrix[3]}\n{
+                        matrix[4]}\n{
+                            matrix[5]}\n{
+                                matrix[6]}\n{
+                                    matrix[7]}\n")
         choice_Play(mtx_data_ava)
     else:
         clearConsole()
-        matrix[row_guess ][column_guess] = u"\u2705"
+        matrix[row_guess][column_guess] = u"\u2705"
         print("  1  |  2 |  3  |  4 |  5 | 6  | 7 | 8")
-        print(f"1{matrix[0]}\n2{matrix[1]}\n3{matrix[2]}\n4{matrix[3]}\n5{matrix[4]}\n6{matrix[5]}\n7{matrix[6]}\n8{matrix[7]}\n")
+        print(
+            f"1{
+                matrix[0]}\n2{
+                matrix[1]}\n3{
+                matrix[2]}\n4{
+                    matrix[3]}\n5{
+                        matrix[4]}\n6{
+                            matrix[5]}\n7{
+                                matrix[6]}\n8{
+                                    matrix[7]}\n")
         choice_Play(mtx_data_ava)
-    
-    #print(matrix[row_guess ][column_guess])
-
-
-
-
-
-
-
-
-
 
 
 def clearConsole():
@@ -217,7 +234,7 @@ def clearConsole():
     command = "clear"
     if os.name in ("nt", "dos"):  # If Machine is running on Windows, use cls
         command = "cls"
-    os.system(command)  
+    os.system(command)
+
 
 main()
-
